b19e478d6aa8198c3382649dabb263fc392fd171
dbranch6 <dbranch6@cnm.edu>
2017-08-03 10:35:37 -0600

    Changed team name

--- php/classes/Quote.php
+++ php/classes/Quote.php
@@ -127,7 +127,7 @@ class Quote implements \JsonSerializable {
 		if(empty($newQuote) === true) {
 			throw (new \InvalidArgumentException("quote is empty or insecure"));
 		}
-		if(strlen($newQuote) < 223) {
+		if(strlen($newQuote) <0) {
 			throw(new \RangeException("quote is too long"));
 		}
 		$this->quote = $newQuote;
