5ae74227b7802d2e30756a56065f102e9b0435c4
dbranch6 <dbranch6@cnm.edu>
2017-08-03 11:30:19 -0600

    Changed team name

--- php/classes/Test/QuoteTest.php
+++ php/classes/Test/QuoteTest.php
@@ -200,7 +200,7 @@ class QuoteTest extends QuoteTestSetup {
 
 		$numRows = $this->getConnection()->getRowCount("quote");
 		//create a quote object and insert it into the database
-		$quote = new Quote(null, $this->$quote->getQuoteId(),
+		$quote = new QuoteId(null, $this->$quote->getQuoteId(),
 			$this->VALID_QUOTE_ID,$this->VALID_GREAT_QUOTE, $this->VALID_QUOTE_AUTHOR, $this->VALID_QUOTE_POSTER, $this->VALID_QUOTE_RATING);
 
 		//insert the quote into the database
